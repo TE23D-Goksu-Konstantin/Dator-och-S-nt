0a902b613963402c27d7c22d48294becda4be70e
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-03-14 14:39:24 +0100

    försökte att fixa så att om man har fler än en av samma grej i kundvagnen så står det 2x istället för att skriva ut den igen men misslyckades

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-function add_to_cart()
+function lägg_till_kundvagn()
 {
     console.log("An Item Has Been Added To The Cart");
 }
+
+let totalaSum = 0;  //definierar den totala summan för produkterna
+
+const produktList = [];
+
+const check = console.log(produktList.length === new Set(produktList).size)
+
+function lägg_till_kundvagn(pris, produktNamn)  //lägger till produkterna i kundvagnen (parameter)
+{    
+    if(produktNamn == "CPU","GPU","HDD","SSD","Moderkort","PSU")
+        {
+            produktList.push(produktNamn);
+            console.log(produktNamn + " har lagts till i varukorgen");
+        }
+
+    if(produktList.includes("CPU","GPU","HDD","SSD","Moderkort","PSU") || check)
+        {
+            totalaSum += pris;
+            document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; //uppdaterar summa-classen
+
+            let sakerList = document.querySelector(".saker"); 
+        
+            let produktSak = document.createElement("p"); //lägger till en ny rad av elementet p för at se till att kundvagnen har saken man laggt till
+            produktSak.innerText = "2x" + produktNamn + " - " + pris + ":-";  
+        
+            sakerList.appendChild(produktSak);
+        }
+    else
+        {
+    
+    totalaSum += pris;  
+    document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; //uppdaterar summa-classen
+
+    let sakerList = document.querySelector(".saker"); 
+
+    let produktSak = document.createElement("p"); //lägger till en ny rad av elementet p för at se till att kundvagnen har saken man laggt till
+    produktSak.innerText = produktNamn + " - " + pris + ":-";  
+
+    sakerList.appendChild(produktSak); //appendar produkten till kundvagnen
+
+
+    }
+}
+
+
+
+
+
+
