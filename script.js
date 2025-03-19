cf27b354e439dd52a12a35c9761cc12b16f92600
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-03-19 18:27:11 +0100

    fixade JSON och localStorage till kundvagnen

--- script.js
+++ script.js
@@ -3,51 +3,59 @@ function lägg_till_kundvagn()
     console.log("An Item Has Been Added To The Cart");
 }
 
-let totalaSum = 0;  //definierar den totala summan för produkterna
 
+let totalaSum = 0;  //definierar den totala summan för produkterna
 const produktList = [];
 
-const check = console.log(produktList.length === new Set(produktList).size)
+function ladda_kundvagn()
+{
+    const sparadProduktList = localStorage.getItem("kundvagn");
 
-function lägg_till_kundvagn(pris, produktNamn)  //lägger till produkterna i kundvagnen (parameter)
-{    
-    if(produktNamn == "CPU","GPU","HDD","SSD","Moderkort","PSU")
+    if (sparadProduktList)
         {
-            produktList.push(produktNamn);
-            console.log(produktNamn + " har lagts till i varukorgen");
+            const jsonKundvagn = JSON.parse(sparadProduktList);
+            totalaSum = jsonKundvagn.totalaSum;
+
+            document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; 
+            jsonKundvagn.produktList.forEach(produkt => {
+                lägg_till_produkt_i_element(produkt.namn, produkt.pris);
+                produktList.push(produkt);
+            });
         }
 
-    if(produktList.includes("CPU","GPU","HDD","SSD","Moderkort","PSU") || check)
-        {
-            totalaSum += pris;
-            document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; //uppdaterar summa-classen
-
-            let sakerList = document.querySelector(".saker"); 
-        
-            let produktSak = document.createElement("p"); //lägger till en ny rad av elementet p för at se till att kundvagnen har saken man laggt till
-            produktSak.innerText = "2x" + produktNamn + " - " + pris + ":-";  
-        
-            sakerList.appendChild(produktSak);
-        }
-    else
-        {
-    
-    totalaSum += pris;  
-    document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; //uppdaterar summa-classen
-
-    let sakerList = document.querySelector(".saker"); 
-
-    let produktSak = document.createElement("p"); //lägger till en ny rad av elementet p för at se till att kundvagnen har saken man laggt till
-    produktSak.innerText = produktNamn + " - " + pris + ":-";  
-
-    sakerList.appendChild(produktSak); //appendar produkten till kundvagnen
-
-
-    }
 }
 
 
 
+function spara_kundvagn()
+{
+    localStorage.setItem("kundvagn", JSON.stringify({totalaSum, produktList}));
+}
+
+function lägg_till_produkt_i_element(produktNamn, pris)
+{
+    let sakerList = document.querySelector(".saker");
+    let produktSak = document.createElement("p");
+    
+    produktSak.innerText = produktNamn + " - " + pris + " :- ";
+    sakerList.appendChild(produktSak);
+    
+}
+
+
+document.addEventListener("DOMContentLoaded", ladda_kundvagn);
 
 
 
+function lägg_till_kundvagn(pris, produktNamn)  //lägger till produkterna i kundvagnen (parameter)
+{    
+
+    totalaSum += pris;  
+    document.getElementById("summa").innerText = "Total: " + totalaSum + ":-"; //uppdaterar summa-classen
+
+    produktList.push({namn: produktNamn, pris});
+
+    lägg_till_produkt_i_element(produktNamn, pris);
+
+    spara_kundvagn();
+}
